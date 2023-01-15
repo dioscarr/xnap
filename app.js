5061624c28ef998544f91418d3b5d1a53d5b94fd
Dioscar <dioscarr@gmail.com>
2023-01-14 23:07:33 -0500

    nodejs express xnap server

--- app.js
+++ app.js
@@ -1,22 +1,104 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var xnapRouter = require('./routes/xnap');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/xnap', xnapRouter);
-
-module.exports = app;
+const express = require('express');
+const { ObjectId } = require('mongodb');
+const client = require('./db');
+const axios = require("axios");
+const app = express();
+app.get('/', (req, res) => {
+    console.log("GetRecipeSuggestions");
+    try {
+        axios
+        .get(
+            "https://recipexerver.onrender.com/BusinessSearchByLocationCategories?limit=1&state=NY"
+        )
+        .then(async (response) => {
+            console.log(response.data);
+            const lead = {xname:'new business',xphone:'3475445544',xurl:`${response.data[0]}`};
+            client.db("xbusiness")
+                    .collection("lead")
+                    .insertOne(lead).then(result => {
+                        res.status(201).json({
+                        message: `Successfully inserted lead: ${result.insertedId}`
+                    });
+        })
+      .catch(err => {
+        res.status(500).json({
+          message: err
+        });
+      });
+    });  
+    } catch (error) {
+        console.error(error);
+    }
+});
+app.post('/leads', (req, res) => {
+  const lead = req.body;
+  client.db("xbusiness").collection("lead").insertOne(lead)
+    .then(result => {
+      res.status(201).json({
+        message: `Successfully inserted lead: ${result.insertedId}`
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err
+      });
+    });
+});
+app.get('/leads', (req, res) => {
+    client.db("xbusiness").collection("lead").find().toArray()
+      .then(leads => {
+        res.status(200).json(leads);
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err
+        });
+      });
+  });
+  app.put('/leads/:id', (req, res) => {
+    const id = req.params.id;
+    const newData = req.body;
+    client.db("xbusiness").collection("lead").updateOne({ _id: ObjectId(id) }, { $set: newData })
+      .then(result => {
+        if (result.matchedCount > 0) {
+          res.status(200).json({
+            message: `Successfully updated lead with id: ${id}`
+          });
+        } else {
+          res.status(404).json({
+            message: `No lead found with id: ${id}`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err
+        });
+      });
+  });
+  app.delete('/leads/:id', (req, res) => {
+    const id = req.params.id;
+    client.db("xbusiness").collection("lead").deleteOne({ _id: ObjectId(id) })
+      .then(result => {
+        if (result.deletedCount > 0) {
+          res.status(200).json({
+            message: `Successfully deleted lead with id: ${id}`
+          });
+        } else {
+          res.status(404).json({
+            message: `No lead found with id: ${id}`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: err
+        });
+      });
+  });
+    
+  
+app.listen(3000, () => {
+    console.log('http://localhost:3000/users');
+  });
+  
---
+++ db.js
@@ -0,0 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+const uri = `${process.env.process.env.MONGODB_CONNECTION}`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect()
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+module.exports = client;
