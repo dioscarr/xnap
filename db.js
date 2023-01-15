fc9a343af9cbe3bd89d3fc395c1f096c96d650bf
Dioscar <dioscarr@gmail.com>
2023-01-15 17:00:43 -0500

    .... db

--- db.js
+++ db.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = `${process.env.MONGODB_CONNECTION}`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri =  `${process.env.MONGODB_CONNECTION}`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect()
-  .then(() => {
-    console.log("Connected to MongoDB Atlas");
-  })
-  .catch(err => {
-    console.log(err);
-  });
+.then(() => {
+  console.log("Connected to MongoDB Atlas");
+})
+.catch(err => {
+  console.log(err);
+});
 
 module.exports = client;
